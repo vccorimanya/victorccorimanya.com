f6baa2952072b54a89281544ff95f2af3e235da0
VictorCcorimanya <raulccorimanyaalfaro@gmail.com>
2021-12-30 21:15:02 -0500

    [ADD] static-data

---
+++ src/static-data/blog.js
@@ -0,0 +1,37 @@
+const datos = [
+  {
+    id:1,
+    title: 'Algoritmos y estructuras de datos',
+    subtitle: 'sdasd',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Soluta voluptate sequi magni culpa dicta consequatur, consectetur
+             illo laborum eius harum reprehenderit accusamus voluptatem ea.
+             Officia quas eius repellat modi repudiandae.
+            `,
+    link: '/blog-content',
+  },
+  {
+    id:2,
+    title: 'Algoritmos y estructuras de datos',
+    subtitle: 'sdasd',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Soluta voluptate sequi magni culpa dicta consequatur, consectetur
+             illo laborum eius harum reprehenderit accusamus voluptatem ea.
+             Officia quas eius repellat modi repudiandae.
+            `,
+    link: '/blog-content',
+  },
+  {
+    id:3,
+    title: 'Algoritmos y estructuras de datos',
+    subtitle: 'sdasd',
+    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Soluta voluptate sequi magni culpa dicta consequatur, consectetur
+             illo laborum eius harum reprehenderit accusamus voluptatem ea.
+             Officia quas eius repellat modi repudiandae.
+            `,
+    link: '/blog-content',
+  },
+];
+
+export default datos;
---
+++ src/static-data/projects.js
@@ -0,0 +1,23 @@
+const projectsData = [
+  {
+    id: 1,
+    title: 'Platzi store',
+    description: 'Este proyecto fue parte de la carrera de python en platzi',
+    url: 'https://github.com/vccorimanya/django-app',
+  },
+  {
+    id: 2,
+    title: 'TNET',
+    description: 'Este proyecto fue parte del curso de desarrollo web de leonidasestaban.com',
+    url: 'https://github.com/vccorimanya/tenet-project',
+  },
+  {
+    id: 3,
+    title: 'Easy Landing',
+    description: 'Diseño realizado por FreeCodeCamp e implementado por Raul Ccorimanya',
+    url: 'https://github.com/vccorimanya/easylanding',
+  }
+
+]
+
+export default projectsData;
