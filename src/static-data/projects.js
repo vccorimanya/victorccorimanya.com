996384332e4ce728d20880fbefd6fc4a8e75c55c
VictorCcorimanya <raulccorimanyaalfaro@gmail.com>
2022-01-11 11:07:21 -0500

    add more projects

--- src/static-data/projects.js
+++ src/static-data/projects.js
@@ -8,6 +8,30 @@ const projectsData = [
   },
   {
     id: 2,
+    title: 'Spotifu',
+    description: 'Es un clon de la pagina de desktop de Spotfy.',
+    url: 'https://vccorimanya.github.io/spotifu/',
+    codigo: 'https://github.com/vccorimanya/spotifu',
+
+  },
+  {
+    id: 3,
+    title: 'Portafolio de Elisa',
+    description: 'Página para mostrar los proyectos de Elisa. Diseño implementado por (leonidasesteban.com)',
+    url: 'https://vccorimanya.github.io/elisa-portfolio/',
+    codigo: 'https://github.com/vccorimanya/elisa-portfolio',
+
+  },
+  {
+    id: 4,
+    title: 'Landing Page',
+    description: 'Landing page para un banco. Diseño realizado por (frontendmentor.io)',
+    url: 'https://vccorimanya.github.io/easylanding/',
+    codigo: 'https://github.com/vccorimanya/easylanding',
+
+  },
+  {
+    id: 5,
     title: 'TNET',
     description: 'TNET es una página de películas para dispositivos móviles, se utilizó hmtl y css.',
     url: 'https://vccorimanya.github.io/tenet-project/',
@@ -15,13 +39,12 @@ const projectsData = [
 
   },
   {
-    id: 3,
+    id: 6,
     title: 'Busca gatitos',
     description: 'En esta página podrás encontrar un buscador simple de gatitos que utiliza la api de Pexels',
     url: 'https://vccorimanya.github.io/search-cats/index.html',
     codigo: 'https://github.com/vccorimanya/search-cats',
   }
-
 ]
 
 export default projectsData;
